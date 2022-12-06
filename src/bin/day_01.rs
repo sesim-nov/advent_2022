398ee6a956b8dcff9f5f3dcb65d9a0fed6819020
Aaron <aaron@atamisk.net>
2022-12-05 20:02:03 -0500

    Added red test for day 1, part 1

--- src/bin/day_01.rs
+++ src/bin/day_01.rs
@@ -3,3 +3,26 @@ use advent_2022::read_file;
 fn main () {
     println!("Hello world!!");
 }
+
+fn day_01(f: &str) -> usize {
+    42
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input() {
+        // Arrange
+        let in_file = "test_input/01.input";
+        let expect_file = "test_input/01.expect";
+
+        // Act
+        let lhs = day_01(in_file);
+        let rhs = read_file::get_lines(expect_file).next().unwrap().unwrap().parse().unwrap();
+
+        // Assert
+        assert_eq!(lhs, rhs);
+    }
+}
