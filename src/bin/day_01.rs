2d4556bf9a4515275dc9c06c145a5a97e0121904
Aaron <aaron@atamisk.net>
2022-12-05 19:48:48 -0500

    Initial commit. Start of day 1.

---
+++ src/bin/day_01.rs
@@ -0,0 +1,5 @@
+use advent_2022::read_file;
+
+fn main () {
+    println!("Hello world!!");
+}
---
+++ src/read_file.rs
@@ -0,0 +1,32 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, Lines};
+
+/// Get an iterator that visits a file line-by-line. 
+pub fn get_lines(path: &str) ->  Lines<BufReader<File>> {
+    let buf = get_bufreader(path);
+    buf.lines()
+}
+
+/// Get a simple bufreader
+pub fn get_bufreader(path: &str) ->  BufReader<File> {
+    let f = File::open(path).expect("Error opening file.");
+    let buf = BufReader::new(f);
+    buf
+}
+
+pub fn read_to_string(path: &str) -> Result<String, &'static str> {
+    let mut bufreader = get_bufreader(path);
+    let mut buf = String::new();
+    match bufreader.read_to_string(&mut buf) {
+        Ok(_) => Ok(buf),
+        Err(_) => Err("UTF error in file.")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
