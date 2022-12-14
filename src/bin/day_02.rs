9c4244a50870f151c90de383e3f13b514d5d9af0
Aaron Moore <aaron@atamisk.net>
2022-12-13 23:58:24 -0500

    Added red test for part 1.

--- src/bin/day_02.rs
+++ src/bin/day_02.rs
@@ -1,3 +1,33 @@
+use advent_2022::read_file;
+
 fn main() {
     println!("Hello world!");
+}
+
+fn part_01(path: &str) -> usize {
+    42
+}
+
+#[cfg(test)]
+mod day_02 {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        // Arrange
+        let in_file = "test_input/02.input";
+        let expect_file = "test_input/02a.expect";
+
+        // Act
+        let lhs = part_01(in_file);
+        let rhs = read_file::get_lines(expect_file)
+            .next()
+            .unwrap()
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        // Assert
+        assert_eq!(lhs, rhs);
+    }
 }
