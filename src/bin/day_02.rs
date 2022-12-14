f01d3c2cdf88211742c8d1f0f504f69ff2b38035
Aaron Moore <aaron@atamisk.net>
2022-12-14 09:14:30 -0500

    Added gameplay logic

--- src/bin/day_02.rs
+++ src/bin/day_02.rs
@@ -4,6 +4,37 @@ fn main() {
     println!("Hello world!");
 }
 
+#[derive(Eq, PartialEq, Debug)]
+enum GameResult {
+    Win,
+    Draw,
+    Loss
+}
+
+enum RPSChoice {
+    Rock,
+    Paper,
+    Scissors
+}
+
+impl RPSChoice {
+    fn play_against(&self, other: &RPSChoice) -> GameResult {
+        use RPSChoice::*;
+        match (self, other) {
+            (Rock, Scissors) | (Paper, Rock) | (Scissors, Paper) => GameResult::Win,
+            (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) => GameResult::Draw,
+            _ => GameResult::Loss,
+        }
+    }
+    fn is_equal(&self, other: &RPSChoice) -> bool {
+        let res = self.play_against(other);
+        match res {
+            GameResult::Draw => true,
+            _ => false
+        }
+    }
+}
+
 fn part_01(path: &str) -> usize {
     42
 }
@@ -11,6 +42,20 @@ fn part_01(path: &str) -> usize {
 #[cfg(test)]
 mod day_02 {
     use super::*;
+    use RPSChoice::*;
+
+    #[test]
+    fn test_gameplay() {
+        assert_eq!(Rock.play_against(&Scissors), GameResult::Win);
+        assert_eq!(Rock.play_against(&Paper), GameResult::Loss);
+        assert_eq!(Rock.play_against(&Rock), GameResult::Draw);
+        assert_eq!(Scissors.play_against(&Paper), GameResult::Win);
+        assert_eq!(Scissors.play_against(&Rock), GameResult::Loss);
+        assert_eq!(Scissors.play_against(&Scissors), GameResult::Draw);
+        assert_eq!(Paper.play_against(&Rock), GameResult::Win);
+        assert_eq!(Paper.play_against(&Scissors), GameResult::Loss);
+        assert_eq!(Paper.play_against(&Paper), GameResult::Draw);
+    }
 
     #[test]
     fn test_part_1() {
