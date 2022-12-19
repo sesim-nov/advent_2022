060f3d946d11771f6c3429c0b627e2eaefee1792
Aaron Moore <aaron@atamisk.net>
2022-12-19 07:12:09 -0500

    Added red test for part 2

--- src/day_03.rs
+++ src/day_03.rs
@@ -12,6 +12,15 @@ pub fn part_01(path:&str) -> usize {
     .sum()
 }
 
+pub fn part_02(path:&str) -> usize {
+    let lines = read_file::get_lines(path);
+    lines.map( |line| {
+        let sack = Rucksack::from(line.unwrap());
+        score_char(sack.get_common()[0])
+    })
+    .sum()
+}
+
 fn score_char(l: char) -> usize {
     let a: usize = if l.is_uppercase() {
         'A' as usize - 26
@@ -33,6 +42,25 @@ mod tests {
 
         // Act
         let lhs = part_01(in_file);
+        let rhs = read_file::get_lines(expect_file)
+            .next()
+            .unwrap()
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        // Assert
+        assert_eq!(lhs, rhs);
+    }
+
+    #[test]
+    fn test_part_02() {
+        // Arrange
+        let in_file = "test_input/03.input";
+        let expect_file = "test_input/03b.expect";
+
+        // Act
+        let lhs = part_02(in_file);
         let rhs = read_file::get_lines(expect_file)
             .next()
             .unwrap()
