0cb23c57094c6897158e416cc3822eecf6715ae8
Aaron <aaron@atamisk.net>
2022-12-20 13:52:12 -0500

    Fmt run

--- src/day_04/section_range.rs
+++ src/day_04/section_range.rs
@@ -2,19 +2,16 @@ use std::str::FromStr;
 
 pub struct SectionRange {
     start: usize,
-    end: usize
+    end: usize,
 }
 
 impl FromStr for SectionRange {
-    type Err = String; 
+    type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let s = Vec::from_iter(s.chars());
         let start: usize = s[0].to_digit(10).unwrap().try_into().unwrap();
         let end: usize = s[2].to_digit(10).unwrap().try_into().unwrap();
-        Ok(Self{
-            start,
-            end,
-        })
+        Ok(Self { start, end })
     }
 }
