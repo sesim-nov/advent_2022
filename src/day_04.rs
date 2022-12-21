0220c0b8af7d4acb5c3b2b9b2b336e977dc16f60
Aaron <aaron@atamisk.net>
2022-12-20 20:44:49 -0500

    Added basics for day 4 part 2

--- src/day_04.rs
+++ src/day_04.rs
@@ -18,6 +18,20 @@ pub fn part_01(path: &str) -> usize {
         .sum()
 }
 
+pub fn part_02(path: &str) -> usize {
+    let lines = read_file::get_lines(path);
+    lines
+        .map(|line| {
+            let ranges: Vec<SectionRange> = line
+                .unwrap()
+                .split(',')
+                .map(|x| SectionRange::from_str(x).unwrap())
+                .collect();
+            (ranges[0].contains(&ranges[1]) || ranges[1].contains(&ranges[0])) as usize
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,6 +41,25 @@ mod tests {
         // Arrange
         let in_file = "test_input/04.input";
         let expect_file = "test_input/04a.expect";
+
+        // Act
+        let lhs = part_01(in_file);
+        let rhs = read_file::get_lines(expect_file)
+            .next()
+            .unwrap()
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        // Assert
+        assert_eq!(lhs, rhs);
+    }
+
+    #[test]
+    fn test_part_02() {
+        // Arrange
+        let in_file = "test_input/04.input";
+        let expect_file = "test_input/04b.expect";
 
         // Act
         let lhs = part_01(in_file);
