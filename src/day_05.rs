2a9a09c9ddd803ad84810f997bffb3d04c8d430e
Aaron Moore <aaron@atamisk.net>
2023-03-06 00:19:41 -0500

    Start of day 5

---
+++ src/day_05.rs
@@ -0,0 +1,54 @@
+use crate::read_file;
+use std::str::FromStr;
+
+
+pub fn part_01(path: &str) -> usize {
+    42
+}
+
+pub fn part_02(path: &str) -> usize {
+    42
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_01() {
+        // Arrange
+        let in_file = "test_input/05.input";
+        let expect_file = "test_input/05a.expect";
+
+        // Act
+        let lhs = part_01(in_file);
+        let rhs = read_file::get_lines(expect_file)
+            .next()
+            .unwrap()
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        // Assert
+        assert_eq!(lhs, rhs);
+    }
+
+    #[test]
+    fn test_part_02() {
+        // Arrange
+        let in_file = "test_input/05.input";
+        let expect_file = "test_input/05b.expect";
+
+        // Act
+        let lhs = part_02(in_file);
+        let rhs = read_file::get_lines(expect_file)
+            .next()
+            .unwrap()
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        // Assert
+        assert_eq!(lhs, rhs);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod day_02;
 pub mod day_03;
 pub mod day_04;
+pub mod day_05;
 pub mod read_file;
