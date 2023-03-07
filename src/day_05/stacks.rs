76364f879bcaf4d2c4f7837dee1abd9b1e9f7f86
Aaron <aaron@atamisk.net>
2023-03-07 13:21:39 -0500

    Added process fn to Stacks

--- src/day_05/stacks.rs
+++ src/day_05/stacks.rs
@@ -1,4 +1,5 @@
 use crate::day_05::parsing;
+use crate::day_05::instruction::Instruction;
 use std::str::FromStr;
 
 pub struct Stacks(Vec<Vec<char>>);
@@ -23,5 +24,38 @@ impl FromStr for Stacks {
             s.reverse();
         }
         Ok(Self(stacks))
+    }
+}
+
+impl Stacks {
+    pub fn process(&mut self, inst: Instruction) {
+        let height = self.0[inst.from].len();
+        let remaining = height.saturating_sub(inst.amount);
+        let mut moving: Vec<char> = self.0[inst.from].drain(remaining..).collect();
+        self.0[inst.to].append(&mut moving);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn process_works() {
+        // Arrange
+        let inst = Instruction {
+            from: 0,
+            to: 1, 
+            amount: 1,
+        };
+        let mut stacks = Stacks(vec![vec!['a', 'b', 'c'], vec!['d', 'e']]);
+
+        // Act
+        stacks.process(inst);
+
+
+        // Assert
+        assert_eq!(stacks.0[0][0], 'a');
+        assert_eq!(stacks.0[1][2], 'c');
     }
 }
