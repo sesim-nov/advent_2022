f409342046983bfbfb0902619eb81dce886964f3
Aaron <1330844+sesim-nov@users.noreply.github.com>
2024-09-14 13:48:32 -0400

    Working on day 05

--- src/day_05.rs
+++ src/day_05.rs
@@ -1,10 +1,15 @@
 use crate::read_file;
 
+struct CrateStacks {
+    stacks: Vec<char>,
+}
+
 pub fn part_01(fname: &std::path::Path) -> String{
-    let lines = read_file::get_lines(&fname.to_str().unwrap());
-    for line in lines {
-        println!("{}", line.unwrap());
-    }
+    let mut lines = read_file::get_lines(&fname.to_str().unwrap());
+    let first_line = lines.next().unwrap().unwrap();
+
+    println!("There are {} columns in this input", (first_line.len() + 1) / 4);
+
     "hi".to_string()
 }
 
