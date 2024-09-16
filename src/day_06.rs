e01cc2cd0d8d6d50f315f876baf8c1216f66ea71
Aaron Moore <1330844+sesim-nov@users.noreply.github.com>
2024-09-15 21:55:32 -0400

    Groundwork for day 06

---
+++ src/day_06.rs
@@ -0,0 +1,50 @@
+use crate::read_file;
+
+pub fn part_01(fname: &std::path::Path) -> String{
+    "stub".to_string()
+}
+  
+
+pub fn part_02(fname: &std::path::Path) -> String{
+    "stub".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+    use super::*;
+
+    #[test]
+    fn test_part_01() {
+        // Arrange
+        let in_file = Path::new("test_input/06.input");
+        let expect_file = "test_input/06a.expect";
+
+        // Act
+        let lhs = part_01(in_file);
+        let rhs = read_file::get_lines(expect_file)
+            .next()
+            .unwrap()
+            .unwrap();
+
+        // Assert
+        assert_eq!(lhs, rhs);
+    }
+
+    // #[test]
+    // fn test_part_02() {
+    //     // Arrange
+    //     let in_file = Path::new("test_input/06.input");
+    //     let expect_file = "test_input/06b.expect";
+
+    //     // Act
+    //     let lhs = part_02(in_file);
+    //     let rhs = read_file::get_lines(expect_file)
+    //         .next()
+    //         .unwrap()
+    //         .unwrap();
+
+    //     // Assert
+    //     assert_eq!(lhs, rhs);
+    // }
+}
