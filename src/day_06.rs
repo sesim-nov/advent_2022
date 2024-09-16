64782e03054f73e353d5610f5a3d74ea3aa326da
Aaron Moore <1330844+sesim-nov@users.noreply.github.com>
2024-09-15 22:33:29 -0400

    Non-working example complete with tests. Have a plan of action in the comments on day 6 lib file.

--- src/day_06.rs
+++ src/day_06.rs
@@ -1,7 +1,35 @@
 use crate::read_file;
 
+fn look_for_packet(pkt: &str) -> i32 {
+    let stream: Vec<char> = pkt.chars().collect();
+
+    let mut packet_stack: Vec<char> = Vec::new();
+    let mut n = -1;
+    
+    //TODO: This method is wrong. I don't need to discard the stack when the check fails. I just need to keep looking. Need to switch to std::slice::Windows. 
+    for c in stream {
+        n += 1;
+        if packet_stack.iter().all(|x| *x != c) {
+            packet_stack.push(c);
+        } else {
+            packet_stack.clear();
+        }
+
+        if packet_stack.len() > 3 {
+            break;
+        }
+    }
+    n
+}
+
 pub fn part_01(fname: &std::path::Path) -> String{
-    "stub".to_string()
+    let pkt = 
+        read_file::read_to_string(fname.to_str().unwrap())
+        .expect("File read error");
+
+    let n = look_for_packet(&pkt);
+
+    n.to_string()
 }
   
 
@@ -30,6 +58,31 @@ mod tests {
         // Assert
         assert_eq!(lhs, rhs);
     }
+
+    #[test]
+    fn test_part_01b() {
+        //Arrange
+        let pkt = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+
+        //Act
+        let n = look_for_packet(pkt);
+
+        //Assert
+        assert_eq!(n, 10);
+    }
+
+    #[test]
+    fn test_part_01c() {
+        //Arrange
+        let pkt = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+
+        //Act
+        let n = look_for_packet(pkt);
+
+        //Assert
+        assert_eq!(n, 5);
+    }
+
 
     // #[test]
     // fn test_part_02() {
