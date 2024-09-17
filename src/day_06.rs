eeaa2295abc0b97c257bc8f6cc1f1531171fb23f
Aaron Moore <1330844+sesim-nov@users.noreply.github.com>
2024-09-16 22:51:26 -0400

    Day 6 part 2 complete

--- src/day_06.rs
+++ src/day_06.rs
@@ -25,7 +25,7 @@ fn look_for_packet(pkt: &str, window_size: usize) -> i32 {
             }
         }
     };
-    res + 4
+    res + i32::try_from(window_size).unwrap()
 }
 
 pub fn part_01(fname: &std::path::Path) -> String{
@@ -40,7 +40,13 @@ pub fn part_01(fname: &std::path::Path) -> String{
   
 
 pub fn part_02(fname: &std::path::Path) -> String{
-    "stub".to_string()
+    let pkt = 
+        read_file::read_to_string(fname.to_str().unwrap())
+        .expect("File read error");
+
+    let n = look_for_packet(&pkt, 14);
+
+    n.to_string()
 }
 
 #[cfg(test)]
@@ -90,20 +96,20 @@ mod tests {
     }
 
 
-    // #[test]
-    // fn test_part_02() {
-    //     // Arrange
-    //     let in_file = Path::new("test_input/06.input");
-    //     let expect_file = "test_input/06b.expect";
+    #[test]
+    fn test_part_02() {
+        // Arrange
+        let in_file = Path::new("test_input/06.input");
+        let expect_file = "test_input/06b.expect";
 
-    //     // Act
-    //     let lhs = part_02(in_file);
-    //     let rhs = read_file::get_lines(expect_file)
-    //         .next()
-    //         .unwrap()
-    //         .unwrap();
+        // Act
+        let lhs = part_02(in_file);
+        let rhs = read_file::get_lines(expect_file)
+            .next()
+            .unwrap()
+            .unwrap();
 
-    //     // Assert
-    //     assert_eq!(lhs, rhs);
-    // }
+        // Assert
+        assert_eq!(lhs, rhs);
+    }
 }
