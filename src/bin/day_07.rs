36a5448191e6df2ecca02246e8582b54849cab53
Aaron Moore <1330844+sesim-nov@users.noreply.github.com>
2024-09-17 22:48:23 -0400

    start of day 07

---
+++ src/bin/day_07.rs
@@ -0,0 +1,10 @@
+use advent_2022::day_07::*;
+
+fn main() {
+    let path = std::path::Path::new("input/07.txt");
+    let part_1 = part_01(path);
+    //let part_2 = part_02(path);
+
+    println!("Part 1: {}", part_1);
+    //println!("Part 2: {}", part_2);
+}
